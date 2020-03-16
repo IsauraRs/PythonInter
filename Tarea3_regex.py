6d62e0c6d0a93a88691942d5428585ed068c08cb
IsauraRs <isaurars97@gmail.com>
2020-03-15 20:21:02 -0600

    Delete Tarea3_regex.py

--- Tarea3_regex.py
+++
@@ -1,9 +0,0 @@
-import re
-c=input("Ingrese la contraseña: ")
-
-
-if re.findall('^[a-z]{3}[0-9]{2}([A-Z]|[a-z]){3}(.|\s|\S|\d){1}$',c):
-    print(("Bienvenido"))
-
-else:
-    print("La contraseña no cumple con el formato requerido")
