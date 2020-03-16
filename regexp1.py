31dc8afd9c777dbdba007f4152f5e005ee932090
IsauraRs <isaurars97@gmail.com>
2020-03-15 20:43:03 -0600

    Delete regexp1.py

--- regexp1.py
+++
@@ -1,8 +0,0 @@
-import re
-instructores = ["Karina Flores","Alejandro Herrera","Héctor Cabello","Stephanie Cabello"]
-
-for i in instructores:
-    if re.findall('Cabello$',i):
-        print(i)
-    else:
-        print("No hubo coincidencia")
