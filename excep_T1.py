6b6739c53ba41535cebc019a7c7c63dc4eea7485
IsauraRs <isaurars97@gmail.com>
2020-03-15 20:34:58 -0600

    Delete excep_T1.py

--- excep_T1.py
+++
@@ -1,5 +0,0 @@
-class ErrorNoEsMultiplo(Exception):
-    def __init__(self,valor):
-        self.valorError = valor
-    def __str__(self):
-        print("Los números que ingresó no son múltiplos ", self.valorError)
